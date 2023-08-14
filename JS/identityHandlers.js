b724f408f237c0b91ed13866dddb8cafc7657e08
Dominic Castaneda <DomSecured@protonmail.com>
2023-08-14 19:16:31 -0400

    Update identityHandlers.js

--- JS/identityHandlers.js
+++ JS/identityHandlers.js
@@ -1,33 +1,39 @@
-function initializeIdentityHandlers() {
+function setLoginState(isLoggedIn) {
     let authControl = document.getElementById('authControl');
     if (authControl) {
-        authControl.addEventListener('click', function(event) {
-            event.preventDefault();
-            if (window.netlifyIdentity.currentUser()) {
-                netlifyIdentity.logout();
-            } else {
-                netlifyIdentity.open();
-            }
-        });
+        if (isLoggedIn) {
+            localStorage.setItem('isLoggedIn', 'true');
+            authControl.innerHTML = `<a href="#" onclick="event.preventDefault(); netlifyIdentity.logout();">Logout</a>`;
+        } else {
+            localStorage.removeItem('isLoggedIn');
+            authControl.innerHTML = `<a href="#" onclick="event.preventDefault(); netlifyIdentity.open();">Login</a>`;
+        }
     }
-    
-    window.netlifyIdentity.on('login', function(user) {
-        console.log('User has logged in:', user);
-        if (authControl) {
-            authControl.innerHTML = `<a href="#" onclick="event.preventDefault();">Logout</a>`;
-        }
-    });
-
-    window.netlifyIdentity.on('logout', function() {
-        console.log('User has logged out.');
-        if (authControl) {
-            authControl.innerHTML = `<a href="#" onclick="event.preventDefault();">Login</a>`;
-        }
-    });
 }
 
-// If you're using an asynchronous method in loadNavbar.js, 
-// make sure to call initializeIdentityHandlers() after loading the navbar there.
-// For now, I'll call it here to handle cases when navbar is loaded without asynchronous methods.
-initializeIdentityHandlers();
+window.netlifyIdentity.on('init', function(user) {
+    if (user || localStorage.getItem('isLoggedIn') === 'true') {
+        setLoginState(true);
+    } else {
+        setLoginState(false);
+    }
+});
 
+window.netlifyIdentity.on('login', function(user) {
+    console.log('User has logged in:', user);
+    setLoginState(true);
+});
+
+window.netlifyIdentity.on('logout', function() {
+    console.log('User has logged out.');
+    setLoginState(false);
+});
+
+// Call this function in the loadNavbar.js after loading the navbar
+function initializeIdentityHandlers() {
+    if (localStorage.getItem('isLoggedIn') === 'true') {
+        setLoginState(true);
+    } else {
+        setLoginState(false);
+    }
+}
