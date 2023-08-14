d62f850115b72661e2bccca0262da3eebcb09375
Dominic Castaneda <DomSecured@protonmail.com>
2023-08-14 17:00:41 -0400

    Updated

---
+++ JS/loadNavbar.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const navbarContainer = document.getElementById("navbarContainer");
+  
+    fetch("../navbar.html")
+      .then(response => response.text())
+      .then(data => {
+        navbarContainer.innerHTML = data;
+      });
+  });
+  
